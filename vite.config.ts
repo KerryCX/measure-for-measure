454585621d74ac3234ba5abb4be48d16e3115215
Kerry Clements <kerryclements@me.com>
2026-06-25 21:29:49 +0100

    fix: correct setupTests filename in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: "./src/setUpTests.ts",
+    setupFiles: "./src/setupTests.ts",
     globals: true,
     coverage: {
       provider: "v8",
